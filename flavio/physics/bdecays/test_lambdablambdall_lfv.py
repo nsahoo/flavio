ce5cd7ef5f26dac0de7241486dfc77a60fdbe2e3
nsahoo <niladri.sahoo@cern.ch>
2023-03-14 14:21:37 +0000

    [Lb2Lll] minor

--- flavio/physics/bdecays/test_lambdablambdall_lfv.py
+++ flavio/physics/bdecays/test_lambdablambdall_lfv.py
@@ -15,12 +15,12 @@ class TestLb2LllLFV(unittest.TestCase):
         obs_1 = flavio.classes.Observable["BR(Lambdab->Lambdaemu)"]
         obs_2 = flavio.classes.Observable["BR(Lambdab->Lambdamue)"]
         self.assertEqual(obs_1.prediction_central(flavio.default_parameters, wc_sm), 0)
-        # BR(BR(Lambdab->Lambdaemu) should be 4 times larger as Wilson coeff is 2x the mue one                                                                                         
+        # BR(BR(Lambdab->Lambdaemu) should be 4 times larger as Wilson coeff is 2x the mue one                                                             
         self.assertAlmostEqual(
             obs_1.prediction_central(flavio.default_parameters, wc_np)
             /obs_2.prediction_central(flavio.default_parameters, wc_np),
             4.,  places=10)
-        # test for errors                                                                                                                                                                             
+        # test for errors                                                                                                                       
         self.assertEqual(flavio.sm_prediction('BR(Lambdab->Lambdaemu)'), 0)
         self.assertEqual(flavio.sm_prediction('BR(Lambdab->Lambdamue)'), 0)
 
